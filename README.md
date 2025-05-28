84ef73ac04e4958f59428d1b1cd0b934b36eb3ed
Bittu Kumar <bittuk@essbbsr.esspl.com>
2025-05-28 11:26:27 +0530

    form added

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ pip install "uvicorn[standard]"
 
 ### To runserver
 uvicorn main:app --reload
+
+When you need to receive form fields instead of JSON, you can use Form.
+# To use forms, first install python-multipart.
+pip install python-multipart.
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
-from typing import Union # To print default value in query parameter
+from fastapi import FastAPI, Query, Form
+from typing import Union, Annotated # To print default value in query parameter
 from enum import Enum
+from pydantic import BaseModel # To make schema
+
 
 app_name = FastAPI()
 
@@ -29,10 +31,10 @@ async def path_fun1(Item):
 #     var_name = {"name":name,"roll_no":roll_no}
 #     return (var_name)
 
-@app_name.get('/query')
-async def query_fun(roll_no:int=0, name:str="bittu"):
-    var_name = {"name":name,"roll_no":roll_no}
-    return (var_name)
+# @app_name.get('/query')
+# async def query_fun(roll_no:int=0, name:str="bittu"):
+#     var_name = {"name":name,"roll_no":roll_no}
+#     return (var_name)
 
 
 class Choice_Names(str,Enum):
@@ -54,3 +56,48 @@ async def get_model(model_name=Choice_Names):
         return {"model_name":model_name,"message":"Calling two"}
     
     return {"model_name":model_name,"message":"Calling three"}
+
+class Schema1(BaseModel):
+    name:str
+    Class:str
+    roll_no:int
+
+# request body
+@app_name.post('/items/')
+async def create_item(item:Schema1):
+    return item
+
+# @app_name.get('/query')
+# async def query_fun(roll_no:Union[int,None]=None, name:Union[str,None]=None):
+#     var_name = {"name":name,"roll_no":roll_no}
+#     return (var_name)
+
+# Now that we can add some extra validation with Query
+@app_name.get('/query')
+async def query_fun(name:Union[str,None]=None, roll_no:Union[str,None] = Query(default=None, min_length=3, max_length=4)):
+    var_name = {"name":name,"roll_no":roll_no}
+    return (var_name)
+
+# We can't apply min_length and max_length on int variable (roll_no must be str)
+
+# http://127.0.0.1:8000/query?name=bittu&roll_no=9999
+
+# q: str | None = None
+# q : Annotated[str | None] = None
+
+# Both of those versions mean the same thing, q is a parameter that can be a str or None, and by default, it is None.
+
+@app_name.get('/query1')
+async def query_fun1(name : Annotated[str | None, Query(max_length=4)]=None):
+    var_name = {"name":name}
+    return (var_name)
+
+# form data
+@app_name.post("/login/")
+async def login(username : Annotated[str,Form()], password : Annotated[str,Form()]):
+    return {"username":username, "password":password}
+
+@app_name.post('/form/data')
+async def form_data(username:str):
+    return {"username":username}
+
